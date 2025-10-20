785a9887d1dd2a7010284a849b2ece081a51cc37
Ayushman-S-ingh <ayushmansingh40013@gmail.com>
2025-10-20 12:05:46 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/ayushmansingh-portfolio/", // fixed base for GitHub Pages
-  server: {
-    port: 8080,
-    host: "::",
-  },
+  base: "/ayushmansingh-portfolio/", 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 8080,
+    host: "::",
   },
 });
